253db6b95c21d82e330c5678f25cd6e6220e5ff5
Viktor Kulakov <plumking.lol@gmail.com>
2023-08-12 19:02:02 +0300

    Dish navigation buttons refactoring.

--- src/modules/dish/DishMain.jsx
+++ src/modules/dish/DishMain.jsx
@@ -1,35 +1,11 @@
-import { Button, IconButton} from "@mui/material";
 import { DishList } from "src/modules/dish/components/DishList";
-import { useDishStore } from "src/modules/dish/store/index.js";
 import { AddNewDishModal } from "src/modules/dish/components/modal/AddNewDishModal";
-import {ArrowBack} from "@mui/icons-material";
-import {useNavigate} from "react-router-dom";
-import {MainContainer} from "src/shared/ui/MainContainer";
-import {MainButtonBox} from "src/shared/ui/MainButtonBox";
-
-
+import { MainContainer } from "src/shared/ui/MainContainer";
+import { DishTopMenu } from "src/modules/dish/components/DishTopMenu";
 export const DishMain = () => {
-  const setAddDishModalOpen = useDishStore((s) => s.setAddModalOpen);
-  const navigate = useNavigate();
-
-  const handleNavigateToMenu = () => {
-    navigate('/menu')
-  }
-
   return (
     <MainContainer>
-      <IconButton title='Вернуться к меню' onClick={handleNavigateToMenu}>
-        <ArrowBack/>
-      </IconButton>
-      <MainButtonBox>
-        <Button
-          color="success"
-          variant="contained"
-          onClick={setAddDishModalOpen}
-        >
-          Добавить блюдо в меню
-        </Button>
-      </MainButtonBox>
+      <DishTopMenu />
       <DishList />
       <AddNewDishModal />
     </MainContainer>
---
+++ src/modules/dish/components/DishTopMenu.jsx
@@ -0,0 +1,27 @@
+import { Box, IconButton } from "@mui/material";
+import { AddCircleOutlined, ArrowBack } from "@mui/icons-material";
+import { useDishStore } from "src/modules/dish/store/index.js";
+import { useNavigate } from "react-router-dom";
+
+export const DishTopMenu = () => {
+  const setAddDishModalOpen = useDishStore((s) => s.setAddModalOpen);
+  const navigate = useNavigate();
+
+  const handleNavigateToMenu = () => {
+    navigate("/menu");
+  };
+  return (
+    <>
+      <Box display="flex" alignItems="center" mt={2}>
+        <IconButton title="Вернуться к меню" onClick={handleNavigateToMenu}>
+          <ArrowBack />
+        </IconButton>
+      </Box>
+      <Box display="flex" justifyContent="center" mb={2}>
+        <IconButton title="Добавить блюдо в меню" onClick={setAddDishModalOpen}>
+          <AddCircleOutlined color="inherit" />
+        </IconButton>
+      </Box>
+    </>
+  );
+};
