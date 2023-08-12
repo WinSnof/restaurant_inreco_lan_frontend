c67746a7162faff8ed0b4605cd6af381db91fb58
Viktor Kulakov <plumking.lol@gmail.com>
2023-08-12 18:11:40 +0300

    Dish UI refactoring.

--- src/modules/dish/DishMain.jsx
+++ src/modules/dish/DishMain.jsx
@@ -1,9 +1,11 @@
-import {Box, Button, Container, IconButton} from "@mui/material";
+import { Button, IconButton} from "@mui/material";
 import { DishList } from "src/modules/dish/components/DishList";
 import { useDishStore } from "src/modules/dish/store/index.js";
 import { AddNewDishModal } from "src/modules/dish/components/modal/AddNewDishModal";
 import {ArrowBack} from "@mui/icons-material";
 import {useNavigate} from "react-router-dom";
+import {MainContainer} from "src/shared/ui/MainContainer";
+import {MainButtonBox} from "src/shared/ui/MainButtonBox";
 
 
 export const DishMain = () => {
@@ -14,13 +16,12 @@ export const DishMain = () => {
     navigate('/menu')
   }
 
-
   return (
-    <Container my={3} mx={3} sx={{mb:5}}>
+    <MainContainer>
       <IconButton title='Вернуться к меню' onClick={handleNavigateToMenu}>
         <ArrowBack/>
       </IconButton>
-      <Box display="flex" my={3} justifyContent="center">
+      <MainButtonBox>
         <Button
           color="success"
           variant="contained"
@@ -28,9 +29,9 @@ export const DishMain = () => {
         >
           Добавить блюдо в меню
         </Button>
-      </Box>
+      </MainButtonBox>
       <DishList />
       <AddNewDishModal />
-    </Container>
+    </MainContainer>
   );
 };
